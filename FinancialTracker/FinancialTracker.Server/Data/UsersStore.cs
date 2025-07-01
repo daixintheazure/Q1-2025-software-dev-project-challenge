fda3ef7b50c5658044e009bb3484d263d34c2a86
Mario Dickerson <dai.dickerson@gmail.com>
2025-07-01 08:13:22 -0500

    added data folder and usersstore class

---
+++ FinancialTracker/FinancialTracker.Server/Data/UsersStore.cs
@@ -0,0 +1,10 @@
+﻿using FinancialTracker.Server.Models;
+using System.Collections.Concurrent;
+
+namespace FinancialTracker.Server.Data
+{
+    public static class UsersStore
+    {
+        public static ConcurrentDictionary<string, User> Users = new();
+    }
+}
